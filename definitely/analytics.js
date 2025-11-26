c5af150ee5c3b5b81b8445ae72a015cfc6ee147d
waynevernon <wayne.vernon@gmail.com>
2025-11-26 08:12:43 -0600

    fix analytics

--- definitely/analytics.js
+++ definitely/analytics.js
@@ -1,6 +1,13 @@
-!function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2===o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(r=t.createElement("script")).type="text/javascript",r.async=!0,r.src=s.api_host+"/static/array.js",(o=t.getElementsByTagName("script")[0]).parentNode.insertBefore(r,o);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},p="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags reloadFeatureFlags group update_email update_phone update_name update_advertising_id onSessionId".split(" "),n=0;n<p.length;n++)g(u,p[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+// PostHog async snippet
+!function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2===o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(r=t.createElement("script")).type="text/javascript",r.async=!0,r.src=s.api_host+"/static/array.js",(o=t.getElementsByTagName("script")[0]).parentNode.insertBefore(r,o);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},p="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags reloadFeatureFlags group update_email update_phone update_name update_advertising_id onSessionId onLoaded".split(" "),n=0;n<p.length;n++)g(u,p[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
 
 posthog.init("phc_gN0p2azeOOsQGvClnMHU4HBE75jm3v6tCyoZ5o1L8Ue", {
     api_host: "https://us.i.posthog.com",
     person_profiles: "identified_only"
+});
+
+// Log readiness for quick debugging in the console
+posthog.onLoaded(() => {
+    // eslint-disable-next-line no-console
+    console.info("PostHog loaded");
 });
